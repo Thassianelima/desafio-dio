0c9492da79b6a06cac77ec045c30dd2545173774
Thassiane lima <thassiane321@hotmail.com>
2025-03-21 17:15:36 -0300

    upddate

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('oi')
+//projeto
+
+switch (1000){
+    case 1000:
+        console.log('ferro')
+break
+    case 1001 > 2000:
+        console.log('bronze')
+break
+    case 2001 > 5000:
+        console.log('Prata')
+break
+    case 6001 > 7000:
+        console.log('ouro')
+break
+    case 7001 > 8000:
+        console.log('platina diamante')
+break
+    case 8001 > 9000:
+        console.log('ascendente')
+break
+    case 9001 > 10000:
+        console.log('imortal')
+break
+    default: 
+        console.log (radiante)
+}
